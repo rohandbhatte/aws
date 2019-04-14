830fd370bd5b71e4a0d8eaa180ed7dbe5f183976
Rohan Bhatte <rohan.bhatte@gmail.com>
2019-04-14 22:13:15 +0800

    Version 1.0.3 S3 Operations Added

--- src/main/java/com/rohandbhatte/aws_s3/aws_s3.java
+++ src/main/java/com/rohandbhatte/aws_s3/aws_s3.java
@@ -5,13 +5,26 @@
  */
 package com.rohandbhatte.aws_s3;
 
+import software.amazon.awssdk.core.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+
 /**
  *
  * @author rohan
  */
 public class aws_s3 {
     public static void main(String args[]){
-        System.out.println("Working on S3");
+        
+        Region region = Region.AP_SOUTHEAST_1;
+        S3Client s3 = S3Client.builder().region(region).build(); 
+        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+        ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
+        listBucketsResponse.buckets().stream().forEach(x -> System.out.println(x.name()));
+        
+        System.out.println("completed");
+        
     }
     
 }
